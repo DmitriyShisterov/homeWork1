696022560dfba4d8ede79595447ee83c9e7a2e25
DmitriyShisterov <DmiShisterov@yandex.ru>
2016-07-07 18:26:26 +0400

    07.07.2016
    Попробовал оживить форму. Использовал JQuery 3.0.0.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,32 @@
 /**
  * Created by User on 06.07.2016.
  */
+ function resetForms (){
+ 	$('#user_form').trigger('reset');
+ }
+
+
+ $(function(){
+ 	$('#addNewField').click(function(){
+ 		var tr = $('<tr></tr>'),
+ 		fNameTD = $('<td></td>').append($('#firstName').val()),
+ 		lNameTD = $('<td></td>').append($('#lastName').val()),
+ 		ageTD = $('<td></td>').append($('#age').val()),
+ 		countryTD=$('<td></td>').append($('#country').val()),
+ 		deleteBtn = $('<a href="#">Delete</a>'),
+ 		updateBtn = $ ('<a href="#">Edit</a>'),
+ 		editTD=$('<td></td>').append(deleteBtn," ",updateBtn);
+ 		tr.append(fNameTD)
+ 		tr.append(lNameTD)
+ 		tr.append(ageTD)  
+ 		tr.append(countryTD)
+ 		tr.append(editTD)
+ 		tr.appendTo('tbody','userList')
+ 		deleteBtn.click(function(){tr.remove()})
+ 	})
+
+ 	
+
+
+ })
+
